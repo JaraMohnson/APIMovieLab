ecbf62095010d786e11247bb87e3e0adf7ec955e
JaraMohnson <jaramohnson@gmail.com>
2022-07-26 19:44:34 -0400

    sleepy commit

---
+++ MoviesLab/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesLab.Models;
+using System.Diagnostics;
+
+namespace MoviesLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public MovieDAL movieDAL = new MovieDAL();
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [HttpGet] //run when you first go to page 
+        public IActionResult MovieSearch()
+        {
+            return View();
+        }
+
+
+        [HttpPost] //run when you submit your form - can only get here once they search something
+        public IActionResult MovieSearch(string title)
+        {
+            MovieModel movie = movieDAL.GetMovie(title);
+            return View(movie);
+        }
+
+        [HttpGet]
+        public IActionResult MovieNight()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public IActionResult MovieNight(string title1, string title2, string title3)
+        {
+            List<MovieModel> results = new List<MovieModel>
+            {
+             movieDAL.GetMovie(title1),
+             movieDAL.GetMovie(title2),
+             movieDAL.GetMovie(title3)
+            };
+            return View(results);
+            return View("MovieNight", results);
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MoviesLab/Models/MovieDAL.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace MoviesLab.Models
+{
+    public class MovieDAL
+    {
+        public MovieModel GetMovie(string title)
+
+        {
+            string url = $"http://www.omdbapi.com/?apikey=b22fe7c3&t={title}";
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+            MovieModel result = JsonConvert.DeserializeObject<MovieModel>(JSON);
+            return result;
+        }
+
+
+
+    }
+}
